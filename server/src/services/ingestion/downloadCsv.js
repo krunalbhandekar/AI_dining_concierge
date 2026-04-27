7d3be78f85ba049f538d090bb2ce2e4ff2685eea
Krunal Bhandekar <krunalbhandekar10@gmail.com>
2026-04-27 16:28:53 +0530

    Update downloadCsv.js

--- server/src/services/ingestion/downloadCsv.js
+++ server/src/services/ingestion/downloadCsv.js
@@ -1,3 +1,45 @@
+// const fs = require("fs");
+// const axios = require("axios");
+// const path = require("path");
+
+// const DATA_URL =
+//   "https://huggingface.co/datasets/ManikaSaini/zomato-restaurant-recommendation/resolve/main/zomato.csv"; // update if needed
+
+// const outputPath = path.resolve(__dirname, "../../../data/zomato.csv");
+
+// async function downloadCSV() {
+//   try {
+//     // ensure directory exists
+//     fs.mkdirSync(path.dirname(outputPath), { recursive: true });
+
+//     const writer = fs.createWriteStream(outputPath);
+
+//     const response = await axios({
+//       url: DATA_URL,
+//       method: "GET",
+//       responseType: "stream",
+//     });
+
+//     response.data.pipe(writer);
+
+//     return new Promise((resolve, reject) => {
+//       writer.on("finish", () => {
+//         console.log("CSV downloaded successfully");
+//         resolve();
+//       });
+
+//       writer.on("error", (err) => {
+//         console.error("Error writing file:", err);
+//         reject(err);
+//       });
+//     });
+//   } catch (error) {
+//     console.error("Csv Download failed:", error.message);
+//   }
+// }
+
+// module.exports = { downloadCSV };
+
 const fs = require("fs");
 const axios = require("axios");
 const path = require("path");
@@ -11,6 +53,12 @@ async function downloadCSV() {
   try {
     // ensure directory exists
     fs.mkdirSync(path.dirname(outputPath), { recursive: true });
+
+    // delete old file if exists
+    if (fs.existsSync(outputPath)) {
+      console.log("🗑️ Deleting old CSV...");
+      fs.unlinkSync(outputPath);
+    }
 
     const writer = fs.createWriteStream(outputPath);
 
